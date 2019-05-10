745235cb91eb3eaa672e2a3050191dbca7758c40
limed <limed@mozilla.com>
2019-05-10 11:25:27 -0700

    Refactor code
     - `lifecycle.tf` now removed and put into its own module
     - Remove terraform.tfvars with sane defaults
     - Cleaned up `main.tf` and fixed a couple of terraform `count` bugs

--- eks/data.tf
+++ eks/data.tf
@@ -1,7 +1,3 @@
-data "aws_region" "current" {}
-
-data "aws_caller_identity" "current" {}
-
 # This assume your vpc_id output is called vpc_id
 data "aws_vpc" "this" {
   id = "${data.terraform_remote_state.deploy.vpc_id}"
