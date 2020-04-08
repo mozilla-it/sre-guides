3f18f1aa104609779c9576bd1882a643da884eca
limed <limed@mozilla.com>
2020-04-08 09:41:40 -0700

    Moved some guides folder around, AWS specific deployment goes to aws folder and updated docs a little bit

--- aws/vpc-info/main.tf
+++ aws/vpc-info/main.tf
@@ -1,26 +1,26 @@
-
 # This is an example of how to reference a vpc_id
 # using the itsre-sandbox account as reference
 
 data "terraform_remote_state" "deploy" {
   backend = "s3"
 
-  config{
+  config = {
     bucket = "itsre-state-517826968395"
-    key = "terraform/deploy.tfstate"
+    key    = "terraform/deploy.tfstate"
     region = "eu-west-1"
   }
 }
 
 output "vpc_id" {
-  value = "${data.terraform_remote_state.deploy.vpc_id}"
+  value = data.terraform_remote_state.deploy.outputs.vpc_id
 }
 
 # It's also possible to output subnet ids
 output "public_subnets" {
-  value = "${data.terraform_remote_state.deploy.public_subnets}"
+  value = data.terraform_remote_state.deploy.outputs.public_subnets
 }
 
 output "private_subnets" {
-  value = "${data.terraform_remote_state.deploy.private_subnets}"
+  value = data.terraform_remote_state.deploy.outputs.private_subnets
 }
+
--- deploy-vpc/README.md
+++
@@ -1,3 +0,0 @@
-# deploy-vpc
-If you VPC is deploy via the [itsre-deploy](https://github.com/mozilla-it/itsre-deploy) you can reference several handy outputs via the state file
-
